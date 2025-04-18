aa837b45e35cd77c87b5df16ebea7d94b49553fc
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-04-18 15:20:04 +0530

    Update comm.h

--- exianb/comm.h
+++ exianb/comm.h
@@ -12,9 +12,9 @@ typedef struct _MODULE_BASE {
 } MODULE_BASE, *PMODULE_BASE;
 
 enum OPERATIONS {
-    OP_INIT_KEY = 0x8800,
-    OP_READ_MEM = 0x8801,
-    OP_WRITE_MEM = 0x8802,
-    OP_MODULE_BASE = 0x8803,
-    OP_RW_MEM = 0x8804,
+    OP_INIT_KEY = 0x800,
+    OP_READ_MEM = 0x801,
+    OP_WRITE_MEM = 0x802,
+    OP_MODULE_BASE = 0x803,
+    OP_RW_MEM = 0x804,
 };
