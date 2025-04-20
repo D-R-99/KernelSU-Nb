bae5525be4874ea7a8b4adb2d14a0e4526f63361
CodeExia54 <codeexia97@gmail.com>
2025-04-20 23:03:37 +0530

    Update exianb.c

--- exianb/exianb.c
+++ exianb/exianb.c
@@ -463,7 +463,7 @@ bool Touch(bool isdown, unsigned int x, unsigned int y)
         v[i] = mt->slots[i].abs[9];
 
     int slot = -1;
-    int *id_ptr = null;
+    int *id_ptr = NULL;
 
     if (isdown)
     {
