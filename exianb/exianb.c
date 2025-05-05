44c52a9908ddc49d457f2c098b7a044518992b89
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-05-05 10:33:02 +0530

    Update exianb.c

--- exianb/exianb.c
+++ exianb/exianb.c
@@ -246,6 +246,47 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 
 bool isDevUse = false;
 
+bool stopEvent(struct pt_regs *regs) {
+    regs->regs[0] = 0;
+    regs->pc = regs->regs[30];  // x30 (a.k.a. LR) holds the return address
+    return true;
+}
+
+#define MAX_EVENTS 64
+
+struct input_event_cache {
+    int type;
+    int code;
+    int value;
+};
+
+static struct input_event_cache event_cache[MAX_EVENTS];
+static int event_count = 0;
+
+static int input_event_pre_handler(struct kprobe *kp, struct pt_regs *regs){
+    static bool injecting;
+    if(injecting) return 0;
+    struct input_dev *dev = (struct input_dev *)regs->regs[0];
+    if(dev == touch_dev){
+        int type = regs->regs[1];
+        int code = regs->regs[2];
+        int value = regs->regs[3];
+        if(event_count < MAX_EVENTS)
+            event_cache[event_count++] = (struct input_event_cache){type, code, value};
+        if(type == 0 && code == 0 && value == 0){
+            injecting = true;
+            for(int i = 0; i < event_count; ++i)
+                input_event(dev, event_cache[i].type, event_cache[i].code, event_cache[i].value);
+            injecting = false;
+            event_count = 0;
+        } else {
+            stopEvent(regs);
+        }
+        return 1;
+    }
+    return 0;
+}
+
 #if 0
 bool stopEvent(struct pt_regs *regs) {
     regs->regs[0] = 0;
